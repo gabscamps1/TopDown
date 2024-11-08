2a34c8c0729d78719f71289633b8e0d51b1bcbe4
Gabriel de Campos <gabrielultrasinho@gmail.com>
2024-11-08 09:01:55 -0300

    add sprites e transição
    add sprites de armas e ícones de outras armas na UI e transição após morrer

--- Assets/Scripts/UI/LevelLoader.cs
+++ Assets/Scripts/UI/LevelLoader.cs
@@ -11,9 +11,11 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetMouseButtonDown(0)) {
-        //    LoadNextLevel();
-        ////
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
     }
 
     public void LoadNextLevel() {
